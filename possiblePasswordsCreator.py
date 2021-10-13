e55adc83f4485702e2b876dbee0484becf9e9231
Raphael Rody <64621929+Blitk@users.noreply.github.com>
2021-10-13 17:52:06 -0300

    Add files via upload

---
+++ possiblePasswordsCreator.py
@@ -0,0 +1,29 @@
+import os
+from time import sleep
+cont = 0
+
+with open("stringsBrute.txt", "w") as arquivo:
+    for a in range(97, 123):
+        for b in range(97, 123):
+            for c in range(97, 123):
+                for d in range(97, 123):
+                    for e in range(97, 123):
+                        for f in range(97, 123):
+                            for g in range(97, 123):
+                                for h in range(97, 123):
+                                    for i in range(97, 123):
+                                        for j in range(97, 123):
+                                            for k in range(97, 123):
+                                                for l in range(97, 123):
+                                                    for m in range(97, 123):
+                                                        for n in range(97, 123):
+                                                            for o in range(97, 123):
+                                                                for p in range(97, 123):
+                                                    
+                                                                    arquivo.write(f"{chr(a)}{chr(b)}{chr(c)}{chr(d)}{chr(e)}{chr(f)}{chr(g)}{chr(h)}{chr(i)}{chr(j)}{chr(k)}{chr(l)}{chr(m)}{chr(n)}{chr(o)}{chr(p)}\n")
+                                                                    cont += 1
+                                                                    os.system("cls")
+                                                                    print(f"{cont} senhas escritas\n")
+                                                                    sleep(0.3)
+
+print("\n\nFinalizado com sucesso") 
